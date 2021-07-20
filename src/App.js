aa141f0b31f1a758ef049f7be8b77d830e9c5271
rachel-wong <hello.rachelwong@gmail.com>
2021-07-20 19:30:43 +1000

    Added progress to calculation & added reset function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { TextField, Grid, Typography, Container, Box, Paper, Button, FormControl, InputAdornment, Input, CssBaseline} from '@material-ui/core';
+import { TextField, Grid, Typography, Container, Box, Paper, Button, FormControl, InputAdornment, Input, CssBaseline } from '@material-ui/core';
+import { useState } from 'react';
 import { makeStyles, useTheme, ThemeProvider } from '@material-ui/core/styles'
 
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
@@ -20,6 +21,12 @@ const useStyles = makeStyles((theme) => {
     btngroup: {
       paddingTop: theme.spacing(2),
       paddingBottom: theme.spacing(2)
+    },
+    resultPaper: {
+      display: "flex",
+      flexDirection: "column",
+      height: '100%',
+      justifyContent: 'space-between'
     }
   }
 })
@@ -29,6 +36,31 @@ function App() {
   const classes = useStyles()
   const theme = useTheme()
 
+  const [tip, setTip] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [bill, setBill] = useState(0)
+  const [numPeople, setNumPeople] = useState(0)
+
+  console.log('numPeople:', numPeople)
+  console.log('bill:', bill)
+
+  const calculate = (rate) => {
+    console.log("calculate!", rate)
+    let currentTip = (bill / rate).toFixed(2)
+    let tipPerPerson = currentTip / numPeople
+    console.log('tipPerPerson:', tipPerPerson)
+    let totalPerPerson = ((currentTip + bill) / numPeople).toFixed(2)
+    console.log('totalPerPerson:', totalPerPerson)
+    setTip(tipPerPerson)
+    setTotal(totalPerPerson)
+  }
+
+  const reset = () => {
+    setTip(0)
+    setTotal(0)
+    setBill(0)
+    setNumPeople(0)
+  }
   return (
     <ThemeProvider theme={theme}>
     <Container className={ classes.root}>
@@ -38,6 +70,7 @@ function App() {
               <TextField
                 id="bill" name="bill" label="Bill"
                 variant="filled" fullWidth
+                onChange={ (e) => setBill((Number(e.target.value)).toFixed(2))}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
@@ -48,37 +81,38 @@ function App() {
               />
               <Grid container spacing={2} className={ classes.btngroup}>
                 <Grid item xs={4}>
-                  <Button variant="contained" fullWidth size="large" color="primary">
+                  <Button disableElevation variant="contained" fullWidth size="large" color="primary" onClick={() => calculate(0.05) }>
                     5%
                   </Button>
                 </Grid>
                 <Grid item xs={4}>
-                  <Button variant="contained" fullWidth size="large" color="primary">
+                  <Button disableElevation variant="contained" fullWidth size="large" color="primary" onClick={() => calculate(0.1)}>
                     10%
                   </Button>
                 </Grid>
                 <Grid item xs={4}>
-                  <Button variant="contained" fullWidth size="large" color="primary">
+                  <Button disableElevation variant="contained" fullWidth size="large" color="primary" onClick={() => calculate(0.15)}s>
                     15%
                   </Button>
                 </Grid>
                 <Grid item xs={4}>
-                  <Button variant="contained" fullWidth size="large" color="primary">
+                  <Button disableElevation variant="contained" fullWidth size="large" color="primary" onClick={() => calculate(0.1)}>
                     25%
                   </Button>
                 </Grid>
                 <Grid item xs={4}>
-                  <Button variant="contained" fullWidth size="large" color="primary">
+                  <Button disableElevation variant="contained" fullWidth size="large" color="primary" onClick={() => calculate(0.5)}>
                     50%
                   </Button>
                 </Grid>
                 <Grid item xs={4}>
-                  <Input id="anyPercent" aria-describedby="my-helper-text" />
+                  <Input placeholder="Custom" id="anyPercent" aria-describedby="my-helper-text" />
                 </Grid>
               </Grid>
               <TextField
                 id="numberPeople" label="Number of People"
                 variant="filled" fullWidth
+                onChange={ (e) => setNumPeople(Math.round(e.target.value))}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
@@ -89,8 +123,28 @@ function App() {
               />
             </Grid>
             <Grid item xs={6}>
-              <Paper>
-                <Button fullWidth variant="contained" size="large" color="primary">RESET</Button>
+              <Paper elevation={0} className={classes.resultPaper}>
+                <Box>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography variant="h6">Tip Amount</Typography>
+                    <Typography variant="body2">/ person</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                      <Typography variant="h2" component="h5">${ tip}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography variant="h6">Total</Typography>
+                    <Typography variant="body2">/ person</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                      <Typography variant="h2" component="h5">${ total}</Typography>
+                  </Grid>
+                  </Grid>
+                </Box>
+                <Button disableElevation fullWidth variant="contained" size="large" color="primary" onClick={ reset }>RESET</Button>
               </Paper>
             </Grid>
           </Grid>
